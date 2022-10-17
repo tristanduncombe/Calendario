4b886613798e8e0488c200592a8da036a0d64d72
Tristan <tduncombe747@gmail.com>
2022-10-17 18:02:52 +1000

    feat(backend) & tweaks(frontend)
    Created backend and tweaked front end

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 9000;
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
+
+app.get("/", (request, response) => {
+  response.send("We Did it");
+});
+app.get("/users/", (request, response) => {
+  response.send("users");
+});
+app.get("/login/:email/:password/", async (req: request, res: response) => {
+  try {
+    //get if username matches
+    username = true;
+    //get if password matches
+    password = true;
+
+    if (username & password) {
+      res.json(true);
+    } else {
+      res.json(false);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
